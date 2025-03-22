31d7177e4cad2b46c34b782588dafeb90660566c
Andrewtheprince <andry.mazzo@outlook.com>
2025-03-22 12:36:54 +0100

    finito

--- controller.py
+++ controller.py
@@ -1,12 +1,50 @@
 import time
 import flet as ft
 import model as md
+from view import View
 
 class SpellChecker:
 
-    def __init__(self, view):
-        self._multiDic = md.MultiDictionary
-        self._view = view
+    def __init__(self, v:View):
+        self._multiDic = md.MultiDictionary()
+        self._view = v
+
+    def handleSpellCheck(self, e):
+        frase = self._view._txtIn.value
+        lingua = self._view._tendinaLingua.value
+        modalita = self._view._tendinaModalita.value
+        if frase == "":
+            self._view._txtOut.controls.append(ft.Text(f"Devi prima inserire una frase!", color="red"))
+            self._view.update()
+            return
+        if lingua == "":
+            self._view._txtOut.controls.append(ft.Text(f"Devi prima selezionare una lingua!", color="red"))
+            self._view.update()
+            return
+        if modalita =="":
+            self._view._txtOut.controls.append(ft.Text(f"Devi prima selezionare una modalità!", color="red"))
+            self._view.update()
+            return
+        self._view._txtOut.clean()
+        self._view._txtOut.controls.append(ft.Text(frase))
+        (parole, tempo) = self.handleSentence(frase, lingua, modalita)
+        self._view._txtOut.controls.append(ft.Text(f"Parole errate: {parole}"))
+        self._view._txtOut.controls.append(ft.Text(f"Tempo richiesto dalla ricerca: {tempo}"))
+        self._view._txtIn.value = ""
+        self._view.update()
+
+
+    def handleLinguaSelezionata(self, e):
+        print(f"Selezionato {self._view._tendinaLingua.value}")
+        self._view._txtOut.controls.append(ft.Text(f"Selezionato {self._view._tendinaLingua.value}!", color="green"))
+        self._view.page.update()
+
+
+    def handleModalitaSelezionata(self, e):
+        print(f"Selezionata modalità {self._view._tendinaModalita.value}")
+        self._view._txtOut.controls.append(ft.Text(f"Selezionata modalità {self._view._tendinaModalita.value}!", color="green"))
+        self._view.page.update()
+
 
     def handleSentence(self, txtIn, language, modality):
         txtIn = replaceChars(txtIn.lower())
@@ -29,7 +67,7 @@ class SpellChecker:
                 parole = self._multiDic.searchWordLinear(words, language)
                 for parola in parole:
                     if not parola.corretta:
-                        paroleErrate = paroleErrate + str(parola) + " "
+                        paroleErrate = paroleErrate + str(parola) + " - "
                 t2 = time.time()
                 return paroleErrate, t2 - t1
 
@@ -43,6 +81,7 @@ class SpellChecker:
                 return paroleErrate, t2 - t1
             case _:
                 return None
+
 
 
     def printMenu(self):
--- view.py
+++ view.py
@@ -1,8 +1,13 @@
 import flet as ft
+from flet.core.types import MainAxisAlignment
+
 
 class View(object):
     def __init__(self, page: ft.Page):
-        # Page
+        self._btnAvvia = None
+        self._txtIn = None
+        self._tendinaModalita = None
+        self._txtOut = None
         self._row1 = None
         self._tendinaLingua = None
         self.page = page
@@ -25,27 +30,26 @@ class View(object):
         self.__theme_switch = ft.Switch(label="Light theme", on_change=self.theme_changed)
         self.page.controls.append(
             ft.Row(spacing=30, controls=[self.__theme_switch, self.__title, ],
-                   alignment=ft.MainAxisAlignment.CENTER)
+                   alignment=ft.MainAxisAlignment.START)
         )
 
         # Add your stuff here
 
-        self._tendinaLingua = ft.Dropdown(label="Selezionare lingua", width=500, options=[ft.dropdown.Option("Italiano"), ft.dropdown.Option("Inglese"), ft.dropdown.Option("Spagnolo")])
-        self._row1 = ft.Row(controls=[self._tendinaLingua], alignment=ft.MainAxisAlignment.CENTER)
+        self._tendinaLingua = ft.Dropdown(label="Selezionare lingua", width=600, options=[ft.dropdown.Option("italian"), ft.dropdown.Option("english"), ft.dropdown.Option("spanish")],
+                                          on_change= lambda e: self.__controller.handleLinguaSelezionata(e)
+                                          )
+        row1 = ft.Row(controls=[self._tendinaLingua], alignment=ft.MainAxisAlignment.START)
+        self._tendinaModalita = ft.Dropdown(label="Modalita ricerca", width=150, options=[ft.dropdown.Option("Default"), ft.dropdown.Option("Linear"), ft.dropdown.Option("Dichotomic")],
+                                            on_change=lambda e: self.__controller.handleModalitaSelezionata(e)
+                                            )
+        self._txtIn = ft.TextField(label="Inserire testo", width=300)
+        self._btnAvvia = ft.ElevatedButton("Avvia controllo", on_click=self.__controller.handleSpellCheck, width=130)
+        row2 = ft.Row(controls=[self._tendinaModalita, self._txtIn, self._btnAvvia], alignment=MainAxisAlignment.START, spacing=10)
 
-
-
-
-
-        self.page.add(self._row1)
-
+        self._txtOut = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        row3 = ft.Row(controls=[self._txtOut], alignment=ft.MainAxisAlignment.START)
+        self.page.add(row1, row2, row3)
         self.page.update()
-
-
-
-
-
-
 
     def update(self):
         self.page.update()
@@ -63,4 +67,3 @@ class View(object):
             "Light theme" if self.page.theme_mode == ft.ThemeMode.LIGHT else "Dark theme"
         )
         self.page.update()
-
